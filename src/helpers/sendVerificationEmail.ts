8df7b23c2382ad8b6f57368d9618623165296b9f
anand5090 <ac7607864145@gmail.com>
2024-09-05 00:18:45 +0530

    blue is upda

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -2,7 +2,7 @@ import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
 import { ApiResponse } from '@/types/ApiResponse';
 
-
+//sdgfgdghfkjsffffsdf
 
 export async function sendVerificationEmail(email: string, username: string, verifyCode: string
 ): Promise<ApiResponse> {
